3d8a440627f2612a939e8c9b9f18dee4f4b1aeac
Ryan Tang <cadmiumtrioxide@gmail.com>
2023-08-23 19:12:56 -0500

    did work

--- Monte-Carlo.py
+++ Monte-Carlo.py
@@ -8,42 +8,53 @@ def f(x):
     return np.exp(-(x**2))
 
 # Bounds for the monte carlo simulation
-a = -10
-
-b = 10
+a = -3
+b = 3
 y=1
 
 # Number of points
-n = 1000
+n = 10000
 
-x_sample = (b-a)*np.random.rand(n) - b
-y_sample = y*np.random.rand(n)
+# Trials
+n_trials = 200
 
-y_real = f(x_sample)
+output = []
 
-in_bound = 0
+for _ in range(n_trials):
+    x_sample = (b-a)*np.random.rand(n) - b
+    y_sample = y*np.random.rand(n)
 
-y_in = []
-x_in = []
+    y_real = f(x_sample)
 
-y_out = []
-x_out = []
+    in_bound = 0
 
-for i in range(n):
-    if y_sample[i] < y_real[i]:
-        in_bound+=1
-        y_in.append(y_sample[i])
-        x_in.append(x_sample[i])
-    else:
-        y_out.append(y_sample[i])
-        x_out.append(x_sample[i])
+    y_in = []
+    x_in = []
 
-print(y*(b-a)*in_bound/n)
+    y_out = []
+    x_out = []
 
-x_plot = np.linspace(a,b,10*(b-a))
-plt.plot(x_plot,f(x_plot),c='red')
-plt.scatter(x_in,y_in,c="orange",label="Inside Curve")
-plt.scatter(x_out,y_out,c="blue",label="Outside Curve")
-plt.title(f"Monte Carlo Integration Method with {n} sample points")
-plt.legend()
-plt.show()
+    for i in range(n):
+        if y_sample[i] < y_real[i]:
+            in_bound+=1
+            y_in.append(y_sample[i])
+            x_in.append(x_sample[i])
+        else:
+            y_out.append(y_sample[i])
+            x_out.append(x_sample[i])
+
+    output.append(y*(b-a)*in_bound/n)
+    #print(y*(b-a)*in_bound/n)
+
+    #x_plot = np.linspace(a,b,10*(b-a))
+    #plt.plot(x_plot,f(x_plot),c='red')
+    #plt.scatter(x_in,y_in,c="orange")
+    #plt.scatter(x_out,y_out,c="blue")
+    
+
+print("DONE")
+print(round(np.average(output),3))
+
+#plt.title(f"Monte Carlo Integration Method with {n} sample points")
+#plt.legend()
+#plt.show()
